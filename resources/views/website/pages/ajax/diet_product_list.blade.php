14611871204fcacc7670b89c8ac118898d30177c
ali098765432 <mali383737@gmail.com>
2023-12-01 04:40:42 +0500

    link-click

--- resources/views/website/pages/ajax/diet_product_list.blade.php
+++ resources/views/website/pages/ajax/diet_product_list.blade.php
@@ -4,18 +4,21 @@
             @foreach ($data['products'] as $key => $item)
                 <div class="col-md-4 col-lg-3 pt-4 pb-4">
                     <div class="item">
-                        <div class="work">
-                            @php
-                                $img = $item->image;
-                            @endphp
-                            <div class="img d-flex align-items-end justify-content-center"
-                                style="background-image: url('{{ asset($img) }}');">
-                                <div class="text w-100 text-center">
-                                    <span class="cat ">{{ $item->name }}</span>
-                                    <h3><a href="#">{{ $item->title }}</a></h3>
+                        <a href="{{ route('product.details', $item->id) }}">
+                            <div class="work">
+
+                                @php
+                                    $img = $item->image;
+                                @endphp
+                                <div class="img d-flex align-items-end justify-content-center"
+                                    style="background-image: url('{{ asset($img) }}');">
+                                    <div class="text w-100 text-center">
+                                        <span class="cat ">{{ $item->name }}</span>
+                                        <h3><a href="{{ route('product.details', $item->id) }}">{{ $item->title }}</a></h3>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 </div>
             @endforeach
